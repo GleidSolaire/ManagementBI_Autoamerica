ac45fedb3ccebf85ed8919befbc94aae1e827890
gleidsolaire <gleids.solaire@yandex.com>
2022-03-24 17:57:18 -0300

    add links embedded

--- painel.php
+++ painel.php
@@ -77,10 +77,10 @@ if(!$_SESSION['usuario']) {
 
                                <div id="demo1" class="collapse bg-dark bg-opacity-25 rounded-2">
                                    <p class="text-muted mx-1">Email: powerbi2@autoamericaimp.onmicrosoft.com</p>
-                                   <p class="text-muted mx-1">Senha: respectiva do acesso  </p>
+                                   <p class="text-muted mx-1">Senha: ***  </p>
                                </div>
                            </p>
-                           <a href="https://app.powerbi.com/links/JbGFbHrOu6?ctid=88f10cf0-be89-4529-9897-8492e0122d92&pbi_source=linkShare" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
+                           <a href="https://app.powerbi.com/reportEmbed?reportId=48e8f53f-2b9b-4805-ae12-7dc15ba5e35f&autoAuth=true&ctid=88f10cf0-be89-4529-9897-8492e0122d92&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D&filterPaneEnabled=false&navContentPaneEnabled=false" target="_blank" class="btn btn-outline-warning rounded rounded-pill">Acessar Dashboard</a>
 
                        </div>
                    </div>
@@ -103,10 +103,10 @@ if(!$_SESSION['usuario']) {
 
                                <div id="demo2" class="collapse bg-dark bg-opacity-25 rounded-2">
                                    <p class="text-muted mx-1">Email: powerbi2@autoamericaimp.onmicrosoft.com</p>
-                                   <p class="text-muted mx-1">Senha: windows</p>
+                                   <p class="text-muted mx-1">Senha: ***</p>
                                </div>
                            </p>
-                           <a href="https://app.powerbi.com/groups/me/reports/10ec52e6-ce58-436a-a43e-163a06578e75/ReportSection" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
+                           <a href="https://app.powerbi.com/reportEmbed?reportId=60d2d1d0-3a4b-4867-8bb5-3b0d02a221df&autoAuth=true&ctid=88f10cf0-be89-4529-9897-8492e0122d92&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D&filterPaneEnabled=false&navContentPaneEnabled=false" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
 
                        </div>
                    </div>
@@ -129,10 +129,10 @@ if(!$_SESSION['usuario']) {
 
                                <div id="demo3" class="collapse bg-dark bg-opacity-25 rounded-2">
                                    <p class="text-muted mx-1">Email: powerbi2@autoamericaimp.onmicrosoft.com</p>
-                                   <p class="text-muted mx-1">Senha: windows</p>
+                                   <p class="text-muted mx-1">Senha: ***</p>
                                </div>
                            </p>
-                           <a href="https://app.powerbi.com/groups/me/reports/b7ee99bf-6da9-406e-8884-94b38aafa315/ReportSection" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
+                           <a href="https://app.powerbi.com/reportEmbed?reportId=b7ee99bf-6da9-406e-8884-94b38aafa315&autoAuth=true&ctid=88f10cf0-be89-4529-9897-8492e0122d92&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D&filterPaneEnabled=false&navContentPaneEnabled=false" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
 
                        </div>
                    </div>
@@ -147,25 +147,50 @@ if(!$_SESSION['usuario']) {
                                <img src="assets/cost.png" class="bg-gradient rounded" style="width:50px;" class="rounded">
                            </div>
                            <h5 class="card-title">
-                                Gastos com Manutenção de Software/SMS
+                                Gastos c/ Manutenção de Software/SMS
                            </h5>
 
                            <p class="card-text ">
 
-                               <a href="#demo3" class=" text-light" data-bs-toggle="collapse">Credenciais:</a>
+                               <a href="#demo4" class=" text-light" data-bs-toggle="collapse">Credenciais:</a>
 
-                               <div id="demo3" class="collapse bg-dark bg-opacity-25 rounded-2">
+                               <div id="demo4" class="collapse bg-dark bg-opacity-25 rounded-2">
                                    <p class="text-muted mx-1">Email: powerbi2@autoamericaimp.onmicrosoft.com</p>
-                                   <p class="text-muted mx-1">Senha: windows</p>
+                                   <p class="text-muted mx-1">Senha: ***</p>
                                </div>
                            </p>
-                           <a href="https://app.powerbi.com/groups/0c18ed7e-b608-4a24-ba37-f029e81bda99/reports/4893b508-43f7-46ba-8fda-0b027577627e/ReportSection" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
+                           <a href="https://app.powerbi.com/reportEmbed?reportId=4893b508-43f7-46ba-8fda-0b027577627e&autoAuth=true&ctid=88f10cf0-be89-4529-9897-8492e0122d92&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D&filterPaneEnabled=false&navContentPaneEnabled=false" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
 
                        </div>
                    </div>
 
                </div>
+               <div class="col-md-4 mt-4  text-white ">
+                   <div class=" card bg-light bg-opacity-25">
 
+                       <div class="card-body">
+                           <div class="card-header text-center">
+                               <img src="assets/financial-profit.png" class="bg-gradient rounded" style="width:50px;" class="rounded">
+                           </div>
+                           <h5 class="card-title">
+                                Relatório de Vendas
+                           </h5>
+
+                           <p class="card-text ">
+
+                               <a href="#demo5" class=" text-light" data-bs-toggle="collapse">Credenciais:</a>
+
+                               <div id="demo5" class="collapse bg-dark bg-opacity-25 rounded-2">
+                                   <p class="text-muted mx-1">Email: powerbi2@autoamericaimp.onmicrosoft.com</p>
+                                   <p class="text-muted mx-1">Senha: ***</p>
+                               </div>
+                           </p>
+                           <a href="https://app.powerbi.com/reportEmbed?reportId=7b97e8ca-8177-40e9-92c1-e87e7969aac2&autoAuth=true&ctid=88f10cf0-be89-4529-9897-8492e0122d92&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D&navContentPaneEnabled=false" target="_blank" class="btn btn-outline-warning shadow-lg rounded-pill">Acessar Dashboard</a>
+
+                       </div>
+                   </div>
+
+               </div>
 
            </div>
 
@@ -173,7 +198,7 @@ if(!$_SESSION['usuario']) {
    </main>
 </body>
 <footer class="border-top text-center bg-opacity-25 bg-dark ">
-<p class="mt-2 text-muted small"> 2021 © Todos os direitos reservados
+<p class="mt-2 text-muted small"> 2022 © Todos os direitos reservados
         <strong class="small">Autoamerica Ltda</strong>
         <span class="text-muted small"> | Desenvolvido por: Gleidson Almeida</span> <span>  <img src="https://cdn-icons-png.flaticon.com/512/1466/1466134.png" width="1.8%" class="" alt="">  </span>
     </p>
